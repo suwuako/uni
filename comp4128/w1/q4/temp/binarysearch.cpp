823fc462ce4c158a2aa67cce98d39beb86e77702
sw <ronnielow@protonmail.com>
2026-06-08 22:04:11 +1000

    q4, diary

--- comp4128/w1/q4/temp/binarysearch.cpp
+++ comp4128/w1/q4/temp/binarysearch.cpp
@@ -15,28 +15,5 @@ int main() {
 
   sort(&arr[0], &arr[len]);
 
-  bool unsolved = true;
-  int factor = len / 2;
-  int current = factor;
-  while(unsolved) {
-    if (current == 0 && arr[current] == 1) {
-      cout << "found target at index " << current << endl;
-      break;
-    }
-    if (arr[current] == 1 && arr[current - 1] == -1) {
-      cout << "found target at index " << current << endl;
-      break;
-    }
 
-    if (arr[current] > target) {
-      current -= factor;
-    } else if (arr[current] < target) {
-      current += factor;
-    }
-    if (factor > 1) {
-      factor /= 2;
-    } else {
-      factor = 1;
-    }
-  }
 }
